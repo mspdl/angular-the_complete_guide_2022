12831c93d19568cfaf26d8b4ce8fe45b63bed2ad
Morgan Espindola <morgan.espindola@ctw.bmwgroup.com>
2022-05-03 11:27:24 +0100

    Practice 01 - Create two components and use them with different styles

--- practices/src/app/app.module.ts
+++ practices/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { Practice01Component } from './practice01/practice01.component';
+import { SuccessAlertComponent } from './practice01/success-alert/success-alert.component';
+import { WarningAlertComponent } from './practice01/warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    Practice01Component,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ practices/src/app/practice01/practice01.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'practice01',
+  templateUrl: './practice01.component.html',
+  styleUrls: ['./practice01.component.css']
+})
+export class Practice01Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
