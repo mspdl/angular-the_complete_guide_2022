73a006d9f58d34089754c9c1fc983a79550904a3
Morgan Espindola <morgan.espindola@gmail.com>
2022-05-03 18:35:34 +0100

    Styling Elements Dynamically with ngStyle

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -3,12 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [
+    `
+      p {
+        padding: 2rem;
+        border-radius: 0.6rem;
+        font-weight: bold;
+        color: #444;
+        border: 0.2rem solid;
+      }
+    `,
+  ],
 })
 export class ServerComponent {
-  serverId: number = 10;
+  serverId = Math.random() * 100;
   serverStatus = 'offline';
+
+  constructor() {
+    this.serverStatus = this.serverId > 50 ? 'online' : 'offline';
+  }
 
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getBackgroundColor() {
+    return this.serverStatus === 'offline' ? 'peachpuff' : '#B4FF9F';
+  }
+
+  getBorderColor() {
+    return this.serverStatus === 'offline' ? 'red' : 'green';
   }
 }
