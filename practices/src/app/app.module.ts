22c4308cbb239d4110e41a5ebe2ba3888f8992fa
Morgan Espindola <morgan.espindola@ctw.bmwgroup.com>
2022-05-03 11:12:14 +0100

    Create a Practice Project

---
+++ practices/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WarningAlertComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
