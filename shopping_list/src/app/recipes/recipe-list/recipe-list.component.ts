7fe1112a4f496da98d7358dd3c639e92d90a4e6c
Morgan Espindola <morgan.espindola@gmail.com>
2022-05-05 15:51:20 +0100

    Adding Content to the Recipes Components

--- shopping_list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping_list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.module';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'Test Recipe Description',
+      'https://img.buzzfeed.com/tasty-app-user-assets-prod-us-east-1/tips/1d204ad1b42644ae9675a44ed1e6a887.jpeg'
+    ),
+  ];
 }
--- shopping_list/src/app/recipes/recipes.component.ts
+++ shopping_list/src/app/recipes/recipes.component.ts
@@ -1,14 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Recipe } from './recipe.module';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
 })
-export class RecipesComponent implements OnInit {
-  recipes = [];
-
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class RecipesComponent {}
