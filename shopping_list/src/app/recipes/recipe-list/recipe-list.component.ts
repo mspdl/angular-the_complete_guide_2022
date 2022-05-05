877512e98506abc3493ff8e3cf909197f60508d3
Morgan Espindola <morgan.espindola@gmail.com>
2022-05-05 16:22:28 +0100

    [Section 3] Outputting a List of Recipes with ngFor (Teacher concerns)

--- shopping_list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping_list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,9 +9,14 @@ import { Recipe } from '../recipe.module';
 export class RecipeListComponent {
   recipes: Recipe[] = [
     new Recipe(
-      'Test Recipe',
-      'Test Recipe Description',
+      'Ratatouille',
+      'This moreish Mediterranean-style vegetable stew is perfect for a super-healthy midweek supper.',
       'https://img.buzzfeed.com/tasty-app-user-assets-prod-us-east-1/tips/1d204ad1b42644ae9675a44ed1e6a887.jpeg'
+    ),
+    new Recipe(
+      'Vanilla Cake',
+      'A classic butter cake but with Japanese techniques applied for the most plush, soft and moist yellow cake like you’ve never had before.',
+      'https://www.recipetineats.com/wp-content/uploads/2020/08/My-best-Vanilla-Cake_9.jpg'
     ),
   ];
 }
