337a254da6de18191713f81d4ce1f23080b1238d
Morgan Espindola <morgan.espindola@gmail.com>
2022-05-05 16:42:27 +0100

    [Section 3] Working on the ShoppingListComponent

--- shopping_list/src/app/shopping-list/shopping-list.component.ts
+++ shopping_list/src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ShoppingListComponent {
+  ingredients = [];
 }
