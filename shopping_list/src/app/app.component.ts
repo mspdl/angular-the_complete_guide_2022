ae7008473e02a67e13eb0319dbe82ff105c4ba97
Morgan Espindola <morgan.espindola@gmail.com>
2022-05-10 10:49:40 +0100

    [Section 6] Adding Navigation with Event Binding and ngIf (My try)

--- shopping_list/src/app/app.component.ts
+++ shopping_list/src/app/app.component.ts
@@ -3,7 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  showRecipeList = true;
+  showShoppingList = false;
+
+  showTheRecipeList() {
+    this.showRecipeList = true;
+    this.showShoppingList = false;
+  }
+
+  showTheShoppingList() {
+    this.showRecipeList = false;
+    this.showShoppingList = true;
+  }
 }
