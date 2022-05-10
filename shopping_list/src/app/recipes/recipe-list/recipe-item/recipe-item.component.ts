76c2af3ef60422aeaa59054462a58a052a765267
Morgan Espindola <morgan.espindola@gmail.com>
2022-05-10 11:20:42 +0100

    [Section 6] Passing Recipe Data with Property Binding (Teacher's approach)

--- shopping_list/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ shopping_list/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Recipe } from '../../recipe.module';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,5 +7,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent {
-  @Input() recipe;
+  @Input() recipe: Recipe;
 }
