a932eac6d85c92789c0d9ccbd8251776700c9f9a
Morgan Espindola <morgan.espindola@gmail.com>
2022-05-13 18:42:29 +0100

    [Section 10] Adding Ingredients to Recipes

--- shopping_list/src/app/recipes/recipe.services.ts
+++ shopping_list/src/app/recipes/recipe.services.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 @Injectable()
@@ -8,12 +9,24 @@ export class RecipeService {
     new Recipe(
       'Ratatouille',
       'This moreish Mediterranean-style vegetable stew is perfect for a super-healthy midweek supper.',
-      'https://img.buzzfeed.com/tasty-app-user-assets-prod-us-east-1/tips/1d204ad1b42644ae9675a44ed1e6a887.jpeg'
+      'https://img.buzzfeed.com/tasty-app-user-assets-prod-us-east-1/tips/1d204ad1b42644ae9675a44ed1e6a887.jpeg',
+      [
+        new Ingredient('Eggplant', 2),
+        new Ingredient('Roma Tomato', 6),
+        new Ingredient('Yellow Squash', 2),
+        new Ingredient('Zucchini', 2),
+      ]
     ),
     new Recipe(
       'Vanilla Cake',
       'A classic butter cake but with Japanese techniques applied for the most plush, soft and moist yellow cake like you’ve never had before.',
-      'https://www.recipetineats.com/wp-content/uploads/2020/08/My-best-Vanilla-Cake_9.jpg'
+      'https://www.recipetineats.com/wp-content/uploads/2020/08/My-best-Vanilla-Cake_9.jpg',
+      [
+        new Ingredient('Salt', 0.1),
+        new Ingredient('Baking Powder', 2),
+        new Ingredient('Baking Soda', 0.5),
+        new Ingredient('Butter', 5),
+      ]
     ),
   ];
 
