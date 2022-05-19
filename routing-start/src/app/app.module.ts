5b25076473d781c8f15c14579dc951156f03a00f
Morgan Espindola <morgan.espindola@gmail.com>
2022-05-19 18:23:26 +0100

    [Section 11] Redirecting and Wildcard Routes

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersComponent } from "./servers/servers.component";
@@ -13,17 +14,21 @@ import { UsersComponent } from "./users/users.component";
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "users", component: UsersComponent,
-    children: [
-      { path: ":id/:name", component: UserComponent }
-    ],
+  {
+    path: "users",
+    component: UsersComponent,
+    children: [{ path: ":id/:name", component: UserComponent }],
   },
-  { path: "servers", component: ServersComponent,
+  {
+    path: "servers",
+    component: ServersComponent,
     children: [
       { path: ":id", component: ServerComponent },
       { path: ":id/edit", component: EditServerComponent },
     ],
   },
+  { path: "not-found", component: PageNotFoundComponent },
+  { path: "**", redirectTo: "/not-found" },
 ];
 
 @NgModule({
@@ -35,6 +40,7 @@ const appRoutes: Routes = [
     UserComponent,
     EditServerComponent,
     ServerComponent,
+    PageNotFoundComponent,
   ],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [ServersService],
