5599962b262b641b76d871cc7963102ebd238da0
Morgan <morgan.espindola@gmail.com>
2022-06-02 19:31:04 +0100

    [Section 12] Fixing Page Reload Issues

--- shopping_list/src/app/app-routing.module.ts
+++ shopping_list/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: "full" },
-  { path: 'recipes', component: RecipeListComponent },
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', component: RecipesComponent },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
 
