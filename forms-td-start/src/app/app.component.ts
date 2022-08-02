bf3e9645c8b9bbeda2dd214f261374d763c9bf58
Morgan <morgan.espindola@gmail.com>
2022-08-02 22:27:18 +0100

    Section 15: TD: Handling Radio Buttons

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -8,6 +8,9 @@ import { NgForm } from "@angular/forms";
 })
 export class AppComponent {
   @ViewChild("f") signupForm: NgForm;
+  defaultQuestion = "pet";
+  answer = "";
+  genders = ["male", "female"];
 
   // onSubmit(form: NgForm) {
   //   console.log("Submitted!", form);
