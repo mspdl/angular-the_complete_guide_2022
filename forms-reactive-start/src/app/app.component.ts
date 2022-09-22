834f46fe688226e59ff985d7ae045646dcadd7d5
Morgan <morgan.espindola@gmail.com>
2022-09-22 18:34:08 -0300

    Section 15: Reactive: Using Error Codes

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -36,7 +36,7 @@ export class AppComponent implements OnInit {
 
   forbiddenNames(control: FormControl): { [s: string]: boolean } {
     if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
-      return { nameIsForbiden: true };
+      return { nameIsForbidden: true };
     } else {
       return null;
     }
