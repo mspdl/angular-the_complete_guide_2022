b171020df619fcc2641a0759d27d858b3c0cdf54
Morgan <morgan.espindola@gmail.com>
2022-10-04 17:48:57 -0300

    Section 18: GETting Data

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,32 +1,40 @@
 import { HttpClient } from "@angular/common/http";
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedPosts = [];
+  API_URL = "https://ornate-veld-185723-default-rtdb.firebaseio.com/";
 
   constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    this.fetchPosts();
+  }
+
   onCreatePost(postData: { title: string; content: string }) {
     this.http
-      .post(
-        "https://ornate-veld-185723-default-rtdb.firebaseio.com/posts.json",
-        postData
-      )
+      .post(this.API_URL + "posts.json", postData)
       .subscribe((responseData) => {
         console.log(responseData);
       });
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get(this.API_URL + "posts.json")
+      .subscribe((posts) => console.log(posts));
   }
 }
--- http-01-start/src/app/app.module.ts
+++ http-01-start/src/app/app.module.ts
@@ -1,14 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
