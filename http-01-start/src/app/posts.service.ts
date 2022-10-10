483e1aaa4e577f2fd3a904fca2068c17e3f8d2c1
Morgan <morgan.espindola@gmail.com>
2022-10-10 20:41:01 -0300

    Section 18: Setting Headers

--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Subject, throwError } from "rxjs";
 import { map, catchError } from "rxjs/operators";
@@ -29,7 +29,9 @@ export class PostsService {
 
   fetchPosts() {
     return this.http
-      .get<{ [key: string]: Post }>(this.API_URL + "posts.json")
+      .get<{ [key: string]: Post }>(this.API_URL + "posts.json", {
+        headers: new HttpHeaders({ "Custom-Header": "Hello" }),
+      })
       .pipe(
         map((responseData) => {
           const postsArray: Post[] = [];
