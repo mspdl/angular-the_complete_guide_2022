a73104de3cb2c42ae9be188fcb1cee2269f7d974
Morgan <morgan.espindola@gmail.com>
2022-10-13 19:08:51 -0300

    Section 18: Adding Query Params

--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Subject, throwError } from "rxjs";
 import { map, catchError } from "rxjs/operators";
@@ -28,9 +28,13 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append("print", "pretty");
+    searchParams = searchParams.append("custom", "key");
     return this.http
       .get<{ [key: string]: Post }>(this.API_URL + "posts.json", {
         headers: new HttpHeaders({ "Custom-Header": "Hello" }),
+        params: searchParams,
       })
       .pipe(
         map((responseData) => {
