e8948115b286bb2b8e9a408ae576ecb08de820ea
Morgan <morgan.espindola@gmail.com>
2022-10-18 14:41:38 -0300

    Section 19: Storing Recipes

--- shopping_list/src/app/header/header.component.ts
+++ shopping_list/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   collapsed = true;
+
+  constructor(private dataStorageService: DataStorageService){}
+
+  onSaveData(){
+    this.dataStorageService.storeRecipes();
+  }
 }
--- shopping_list/src/app/shared/data-storage.service.ts
+++ shopping_list/src/app/shared/data-storage.service.ts
@@ -1,10 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataStorageService {
+  API_URL = 'https://ng-course-recipe-book-bbf68-default-rtdb.firebaseio.com';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(this.API_URL + '/recipes.json', recipes)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
