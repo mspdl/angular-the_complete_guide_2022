db5d2fd3eea7a9e693a70cfc660f3f7a0fb822e2
Morgan Espindola <morgan.espindola@gmail.com>
2022-10-19 11:37:38 +0100

    Section 19: Transforming Response Data

--- shopping_list/src/app/recipes/recipe.service.ts
+++ shopping_list/src/app/recipes/recipe.service.ts
@@ -8,30 +8,32 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Ratatouille',
-      'This moreish Mediterranean-style vegetable stew is perfect for a super-healthy midweek supper.',
-      'https://img.buzzfeed.com/tasty-app-user-assets-prod-us-east-1/tips/1d204ad1b42644ae9675a44ed1e6a887.jpeg',
-      [
-        new Ingredient('Eggplant', 2),
-        new Ingredient('Roma Tomato', 6),
-        new Ingredient('Yellow Squash', 2),
-        new Ingredient('Zucchini', 2),
-      ]
-    ),
-    new Recipe(
-      'Vanilla Cake',
-      'A classic butter cake but with Japanese techniques applied for the most plush, soft and moist yellow cake like you’ve never had before.',
-      'https://www.recipetineats.com/wp-content/uploads/2020/08/My-best-Vanilla-Cake_9.jpg',
-      [
-        new Ingredient('Salt', 0.1),
-        new Ingredient('Baking Powder', 2),
-        new Ingredient('Baking Soda', 0.5),
-        new Ingredient('Butter', 5),
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Ratatouille',
+  //     'This moreish Mediterranean-style vegetable stew is perfect for a super-healthy midweek supper.',
+  //     'https://img.buzzfeed.com/tasty-app-user-assets-prod-us-east-1/tips/1d204ad1b42644ae9675a44ed1e6a887.jpeg',
+  //     [
+  //       new Ingredient('Eggplant', 2),
+  //       new Ingredient('Roma Tomato', 6),
+  //       new Ingredient('Yellow Squash', 2),
+  //       new Ingredient('Zucchini', 2),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Vanilla Cake',
+  //     'A classic butter cake but with Japanese techniques applied for the most plush, soft and moist yellow cake like you’ve never had before.',
+  //     'https://www.recipetineats.com/wp-content/uploads/2020/08/My-best-Vanilla-Cake_9.jpg',
+  //     [
+  //       new Ingredient('Salt', 0.1),
+  //       new Ingredient('Baking Powder', 2),
+  //       new Ingredient('Baking Soda', 0.5),
+  //       new Ingredient('Butter', 5),
+  //     ]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
