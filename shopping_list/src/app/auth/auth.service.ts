43105541b612e248af598f32685ccd253c76aae2
Morgan <morgan.espindola@gmail.com>
2022-10-21 16:14:05 -0300

    Section 20: Preparing the Signup Request

---
+++ shopping_list/src/app/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  API_KEY = 'AIzaSyBSQGFOYDkCxN_UVLTeCRHfQQT3MRFSRj0';
+  API_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[${this.API_KEY}]`;
+
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>(this.API_URL, {
+      email,
+      password,
+      returnSecureToken: true,
+    });
+  }
+}
