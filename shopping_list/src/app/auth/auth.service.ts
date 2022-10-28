6c70cc47eebd054f85a9d9870a32bc7d2bfd3ce8
Morgan <morgan.espindola@gmail.com>
2022-10-28 19:12:34 -0300

    Section 20: Adding the Token to Outgoing Requests

--- shopping_list/src/app/auth/auth.service.ts
+++ shopping_list/src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Subject, throwError } from 'rxjs';
+import { BehaviorSubject, Subject, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { User } from './user.model';
 
@@ -21,7 +21,7 @@ export class AuthService {
   API_URL_SIGN_UP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.API_KEY}`;
   API_URL_SIGN_IN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.API_KEY}`;
 
-  user = new Subject<User>();
+  user = new BehaviorSubject<User>(null);
 
   constructor(private http: HttpClient) {}
 
