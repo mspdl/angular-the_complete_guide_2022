9f44e483cd644141e65f0168a921a8812b701bb8
Morgan <morgan.espindola@gmail.com>
2022-10-31 16:55:34 -0300

    Section 20: Adding Auto-Login

--- shopping_list/src/app/auth/auth.service.ts
+++ shopping_list/src/app/auth/auth.service.ts
@@ -61,6 +61,27 @@ export class AuthService {
     this.router.navigate(['/auth']);
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExperiationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExperiationDate)
+    );
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   private handleAuthentication(responseData: AuthResponseData) {
     const expirationDate = new Date(
       new Date().getTime() + +responseData.expiresIn * 1000
@@ -72,6 +93,7 @@ export class AuthService {
       expirationDate
     );
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorResponse: HttpErrorResponse) {
