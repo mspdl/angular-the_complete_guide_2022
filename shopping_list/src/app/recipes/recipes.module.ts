62ea079eff6fba81644a711e8ac9e64651d39ac3
Morgan <morgan.espindola@gmail.com>
2022-11-10 21:19:55 -0300

    Section 22: Splitting Modules Correctly

--- shopping_list/src/app/recipes/recipes.module.ts
+++ shopping_list/src/app/recipes/recipes.module.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
@@ -16,6 +18,7 @@ import { RecipesComponent } from './recipes.component';
     RecipeStartComponent,
     RecipeEditComponent,
   ],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
   exports: [
     RecipesComponent,
     RecipeListComponent,
