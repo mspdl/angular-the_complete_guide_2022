41769921830d29d691f430c2908385267c0957a3
Morgan <morgan.espindola@gmail.com>
2022-11-29 19:26:30 -0300

    Section 24: Migrating Another Component

--- 01-starting-project/src/app/app.module.ts
+++ 01-starting-project/src/app/app.module.ts
@@ -5,8 +5,8 @@ import { DetailsComponent } from "./welcome/details/details.component";
 import { WelcomeComponent } from "./welcome/welcome.component";
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent],
-  imports: [BrowserModule, DetailsComponent],
+  declarations: [AppComponent],
+  imports: [BrowserModule, DetailsComponent, WelcomeComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
