f72ba996bc9af86102c7c2c7456f9fe2d38d5ff7
Morgan <morgan.espindola@gmail.com>
2022-12-03 23:28:53 -0300

    Section 24: Routing with Standalone Components

--- 03-starting-with-routing/src/app/app.component.ts
+++ 03-starting-with-routing/src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-
-import { WelcomeComponent } from './welcome/welcome.component';
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { WelcomeComponent } from "./welcome/welcome.component";
 
 @Component({
   standalone: true,
-  imports: [WelcomeComponent],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  imports: [WelcomeComponent, RouterModule],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {}
--- 03-starting-with-routing/src/main.ts
+++ 03-starting-with-routing/src/main.ts
@@ -1,9 +1,8 @@
-import { enableProdMode } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-
-import { AppComponent } from './app/app.component';
-// import { AnalyticsService } from './app/shared/analytics.service';
-import { environment } from './environments/environment';
+import { enableProdMode, importProvidersFrom } from "@angular/core";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app/app-routing.module";
+import { AppComponent } from "./app/app.component";
+import { environment } from "./environments/environment";
 
 if (environment.production) {
   enableProdMode();
@@ -12,5 +11,6 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     // AnalyticsService,
+    importProvidersFrom(AppRoutingModule),
   ],
 });
