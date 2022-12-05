cbebc92ea7eb27bf3a533ba6ff117ad399693d97
Morgan <morgan.espindola@gmail.com>
2022-12-04 21:06:50 -0300

    Section 24: Lazy Loading

--- 03-starting-with-routing/src/app/app-routing.module.ts
+++ 03-starting-with-routing/src/app/app-routing.module.ts
@@ -1,24 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Route, RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
+import { NgModule } from "@angular/core";
+import { Route, RouterModule } from "@angular/router";
 
-import { WelcomeComponent } from './welcome/welcome.component';
+import { WelcomeComponent } from "./welcome/welcome.component";
 
 const routes: Route[] = [
   {
-    path: '',
+    path: "",
     component: WelcomeComponent,
   },
   {
-    path: 'about',
-    component: AboutComponent,
+    path: "about",
+    loadComponent: () =>
+      import("./about/about.component").then((mod) => mod.AboutComponent),
   },
   {
-    path: 'dashboard',
+    path: "dashboard",
     loadChildren: () =>
-      import('./dashboard/dashboard-routing.module').then(
-        (mod) => mod.DashboardRoutingModule
-      ),
+      import("./dashboard/routes").then((mod) => mod.DASHBOARD_ROUTES),
   },
 ];
 
