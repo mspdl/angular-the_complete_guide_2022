6b30b32d64737e47a7fa56f02510a04b2fa2c7b6
Morgan <morgan.espindola@gmail.com>
2022-12-05 21:26:39 -0300

    Section 24: Fixed linter

--- 03-starting-with-routing/src/app/welcome/details/details.component.ts
+++ 03-starting-with-routing/src/app/welcome/details/details.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { AnalyticsService } from 'src/app/shared/analytics.service';
-import { HighlightDirective } from 'src/app/shared/highlight.directive';
+import { AnalyticsService } from "src/app/shared/analytics.service";
+import { HighlightDirective } from "src/app/shared/highlight.directive";
 
 @Component({
   standalone: true,
   imports: [HighlightDirective],
-  selector: 'app-details',
-  templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  selector: "app-details",
+  templateUrl: "./details.component.html",
+  styleUrls: ["./details.component.css"],
 })
 export class DetailsComponent {
   constructor(private analyticsService: AnalyticsService) {}
