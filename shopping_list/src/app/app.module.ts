5e4bc4bc8d6e4c02899261c6ac48cd7761856b28
Morgan <morgan.espindola@gmail.com>
2022-12-10 20:46:47 -0300

    Fixed StoreModule error

--- shopping_list/src/app/app.module.ts
+++ shopping_list/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store/src';
+import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core.module';
