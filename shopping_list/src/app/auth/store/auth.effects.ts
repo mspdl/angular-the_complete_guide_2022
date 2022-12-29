0022dca67b82a37f3bd8f18644777a0f14ee221e
Morgan <morgan.espindola@gmail.com>
2022-12-28 23:16:50 -0300

    Section 25: Effects & Error Handling

--- shopping_list/src/app/auth/store/auth.effects.ts
+++ shopping_list/src/app/auth/store/auth.effects.ts
@@ -1,7 +1,41 @@
-import { Actions, ofType } from '@ngrx/effects';
+import { HttpClient } from '@angular/common/http';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { catchError, map, of, switchMap } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import * as AuthActions from './auth.actions';
 
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
 export class AuthEffects {
-  authLogin = this.actions$.pipe(ofType(AuthActions.LOGIN_START));
-  constructor(private actions$: Actions) {}
+  API_URL_SIGN_UP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`;
+  API_URL_SIGN_IN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`;
+
+  @Effect()
+  authLogin = this.actions$.pipe(
+    ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart) => {
+      return this.http
+        .post<AuthResponseData>(this.API_URL_SIGN_IN, {
+          email: authData.payload.email,
+          password: authData.payload.password,
+          returnSecureToken: true,
+        })
+        .pipe(
+          catchError((error) => {
+            of();
+          }),
+          map((resData) => {
+            of();
+          })
+        );
+    })
+  );
+  constructor(private actions$: Actions, private http: HttpClient) {}
 }
