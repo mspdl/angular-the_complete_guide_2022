5e2ab1a7477ab4730e18c011f063e461e6cf40b9
Morgan <morgan.espindola@gmail.com>
2023-01-09 21:53:40 -0300

    Section 25: Getting Started with NgRx for Recipes

--- shopping_list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping_list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { map, Subscription } from 'rxjs';
+import * as fromApp from '../../store/app.reducer';
 import { Recipe } from '../recipe.model';
-import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -14,17 +15,18 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   subscription: Subscription;
 
   constructor(
-    private recipeService: RecipeService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private store: Store<fromApp.AppState>
   ) {}
 
-
   ngOnInit(): void {
-    this.subscription = this.recipeService.recipesChanged.subscribe((recipes) => {
-      this.recipes = recipes;
-    });
-    this.recipes = this.recipeService.getRecipes();
+    this.subscription = this.store
+      .select('recipes')
+      .pipe(map((recipesState) => recipesState.recipes))
+      .subscribe((recipes) => {
+        this.recipes = recipes;
+      });
   }
 
   onNewRecipe() {
--- shopping_list/src/app/store/app.reducer.ts
+++ shopping_list/src/app/store/app.reducer.ts
@@ -1,13 +1,16 @@
 import { ActionReducerMap } from '@ngrx/store';
 import * as fromAuth from '../auth/store/auth.reducer';
+import * as fromRecipes from '../recipes/store/recipe.reducer';
 import * as fromShoppingList from '../shopping-list/store/shopping-list.reducer';
 
 export interface AppState {
   shoppingList: fromShoppingList.ShoppingListState;
   auth: fromAuth.UserState;
+  recipes: fromRecipes.RecipeState;
 }
 
 export const appReducer: ActionReducerMap<AppState> = {
   shoppingList: fromShoppingList.shoppingListReducer,
   auth: fromAuth.authReducer,
+  recipes: fromRecipes.recipeReducer,
 };
