54439004d2bfe6a75cdb4edc0e017e53d6117063
Morgan <morgan.espindola@gmail.com>
2023-01-18 11:10:13 -0300

    Section 26: Adding Angular Universal

--- shopping_list/src/app/app.component.ts
+++ shopping_list/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as AuthActions from './auth/store/auth.actions';
 import { LoggingService } from './logging.service';
@@ -12,11 +13,14 @@ import * as fromApp from './store/app.reducer';
 export class AppComponent implements OnInit {
   constructor(
     private store: Store<fromApp.AppState>,
-    private logginService: LoggingService
+    private logginService: LoggingService,
+    @Inject(PLATFORM_ID) private platformId
   ) {}
 
   ngOnInit(): void {
-    this.store.dispatch(AuthActions.autoLogin());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(AuthActions.autoLogin());
+    }
     this.logginService.printLog('Hello from AppComponent ngOnInit');
   }
 }
--- shopping_list/src/environments/environment.prod.ts
+++ shopping_list/src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
+const firebaseAPIKey = 'AIzaSyBSQGFOYDkCxN_UVLTeCRHfQQT3MRFSRj0';
+
 export const environment = {
-  production: true,
-  firebaseAPIKey: 'AIzaSyBSQGFOYDkCxN_UVLTeCRHfQQT3MRFSRj0',
+  production: false,
+  API_URL: 'https://ng-course-recipe-book-bbf68-default-rtdb.firebaseio.com',
+  API_URL_SIGN_IN: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${firebaseAPIKey}`,
+  API_URL_SIGN_UP: `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${firebaseAPIKey}`,
 };
