3825d586754a1642f4161bc26e1ace4997e3bc60
Morgan <morgan.espindola@gmail.com>
2023-01-21 16:06:08 -0300

    Section 27: Setting up the Starting Project

---
+++ animations-start/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  list = ['Milk', 'Sugar', 'Bread'];
+
+  onAdd(item: any) {
+    this.list.push(item);
+  }
+
+  onDelete(item: any) {
+    console.log(item);
+  }
+}
