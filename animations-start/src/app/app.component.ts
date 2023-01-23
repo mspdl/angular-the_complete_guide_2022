dbdc7b66e2c598ec8ce5f81da7c8b3b6099386db
Morgan <morgan.espindola@gmail.com>
2023-01-22 21:09:06 -0300

    Section 27: Animations Triggers and State

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -1,10 +1,24 @@
+import { state, style, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state(
+        'normal',
+        style({ backgroundColor: 'red', transform: 'translateX(0)' })
+      ),
+      state(
+        'highlighted',
+        style({ backgroudColor: 'blue', transform: 'translateX(100px)' })
+      ),
+    ]),
+  ],
 })
 export class AppComponent {
+  state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAdd(item: any) {
@@ -12,6 +26,6 @@ export class AppComponent {
   }
 
   onDelete(item: any) {
-    console.log(item);
+    this.list.splice(this.list.indexOf(item), 1);
   }
 }
