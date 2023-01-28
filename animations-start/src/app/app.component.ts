8b78210028fcc705d981ac9c70472778c0536633
Morgan <morgan.espindola@gmail.com>
2023-01-27 23:47:35 -0300

    Section 27: Using Keyframes for Animations

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {
   animate,
+  keyframes,
   state,
   style,
   transition,
@@ -74,6 +75,39 @@ enum AnimationStates {
       transition(`${AnimationStates.Void} => *`, [
         style({ opacity: 0, transform: 'translateX(-100px)' }),
         animate(300),
+      ]),
+      transition(`* => ${AnimationStates.Void}`, [
+        animate(500, style({ opacity: 0, transform: 'translateX(100px)' })),
+      ]),
+    ]),
+    trigger('list2', [
+      state(
+        AnimationStates.In,
+        style({ opacity: 1, transform: 'translateX(0)' })
+      ),
+      transition(`${AnimationStates.Void} => *`, [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 1
+          })
+        ]))
       ]),
       transition(`* => ${AnimationStates.Void}`, [
         animate(500, style({ opacity: 0, transform: 'translateX(100px)' })),
