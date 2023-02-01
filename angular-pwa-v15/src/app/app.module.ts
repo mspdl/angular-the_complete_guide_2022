f9003038c12c12594a427a726bfa5ea04d7f7d11
Morgan <morgan.espindola@gmail.com>
2023-01-31 21:50:21 -0300

    Section 28: Adding Service Workers

--- angular-pwa-v15/src/app/app.module.ts
+++ angular-pwa-v15/src/app/app.module.ts
@@ -1,14 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { isDevMode, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [AppComponent, PostComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- angular-pwa-v15/src/app/post/post.component.spec.ts
+++ angular-pwa-v15/src/app/post/post.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PostComponent } from './post.component';
 
 describe('PostComponent', () => {
@@ -8,9 +7,8 @@ describe('PostComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PostComponent ]
-    })
-    .compileComponents();
+      declarations: [PostComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
