79355d39c048645d00e2529db1c68d0656c3baef
Morgan <morgan.espindola@gmail.com>
2023-02-04 15:07:03 -0300

    Section 29: Running Tests (with the CLI)

--- tests/src/app/app.component.spec.ts
+++ tests/src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -19,13 +17,16 @@ describe('AppComponent', () => {
   it(`should have as title 'tests'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tests');
+    expect(app.title).toEqual('My title');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tests app is running!');
+    const app = fixture.componentInstance;
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      `${app.title} app is running!`
+    );
   });
 });
