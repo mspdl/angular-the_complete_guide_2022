f5533a6443932504b709d252bde0ce96b8cfb432
Morgan <morgan.espindola@gmail.com>
2023-02-21 12:08:46 -0300

    Section 30: Simplified Deployment with "ng deploy"

--- angular-config/src/app/app.module.ts
+++ angular-config/src/app/app.module.ts
@@ -10,6 +10,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -24,9 +35,21 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-config/src/environments/environment.ts
+++ angular-config/src/environments/environment.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'ng-course-recipe-book-bbf68',
+    appId: '1:533029559448:web:03817c09e78414eb6e04ed',
+    databaseURL: 'https://ng-course-recipe-book-bbf68-default-rtdb.firebaseio.com',
+    storageBucket: 'ng-course-recipe-book-bbf68.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBSQGFOYDkCxN_UVLTeCRHfQQT3MRFSRj0',
+    authDomain: 'ng-course-recipe-book-bbf68.firebaseapp.com',
+    messagingSenderId: '533029559448',
+  },};
