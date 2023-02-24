ee057b46071354601fa5ac4b50e2a57199d5f45e
Morgan Espindola <morgan.espindola@gmail.com>
2023-02-24 16:55:03 +0000

    Section 32: Base Types & Primitives

---
+++ typescript/basics.ts
@@ -0,0 +1,20 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number; // Number can be used too, but it's a javaScript object
+
+age = 12;
+age = 12.1;
+// age = "12"; we can't do that
+
+let userName: string;
+userName = "Morgan";
+
+let isInstructor: boolean;
+isInstructor = true;
+
+let hobbies: null;
+// hobbies = 12; not possible
